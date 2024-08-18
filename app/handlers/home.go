730f2a68717c4d51151625a7904b9590416ade6f
kovardin <artem-kovardin@yandex.ru>
2024-08-18 23:11:06 +0300

    added home page

--- app/handlers/home.go
+++ app/handlers/home.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v5"
+	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/apis"
 	"github.com/pocketbase/pocketbase/tools/template"
 
@@ -11,20 +12,72 @@ import (
 )
 
 type Home struct {
+	app      *pocketbase.PocketBase
 	registry *template.Registry
 }
 
-func NewHome(registry *template.Registry) *Home {
+func NewHome(app *pocketbase.PocketBase, registry *template.Registry) *Home {
 	return &Home{
+		app:      app,
 		registry: registry,
 	}
 }
 
+type Artifact struct {
+	Name        string
+	Group       string
+	Description string
+	LastVersion string
+}
+
 func (h *Home) Home(c echo.Context) error {
+	records, err := h.app.Dao().FindRecordsByFilter(
+		"artifacts",
+		"enabled = true",
+		"-created",
+		100,
+		0,
+	)
+
+	if err != nil {
+		h.app.Logger().Error("failed fetch artifacts", "err", err)
+	}
+
+	aa := []Artifact{}
+	for _, record := range records {
+		versions, err := h.app.Dao().FindRecordsByFilter(
+			"versions",
+			"enabled = true",
+			"-created",
+			1,
+			0,
+		)
+
+		if err != nil {
+			h.app.Logger().Error("failed fetch versions", "err", err)
+
+			continue
+		}
+
+		v := ""
+		if len(versions) > 0 {
+			v = versions[0].GetString("version")
+		}
+
+		aa = append(aa, Artifact{
+			Name:        record.GetString("name"),
+			Group:       record.GetString("group"),
+			Description: record.GetString("description"),
+			LastVersion: v,
+		})
+	}
+
 	html, err := h.registry.LoadFS(views.FS,
 		"layout.html",
 		"home/home.html",
-	).Render(map[string]any{})
+	).Render(map[string]any{
+		"artifacts": aa,
+	})
 
 	if err != nil {
 		return apis.NewNotFoundError("", err)
